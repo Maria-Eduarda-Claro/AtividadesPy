84c4412c2da624454578ebf1511e9efae5590595
Maria Eduarda Claro <79665660+Maria-Eduarda-Claro@users.noreply.github.com>
2022-06-08 19:17:45 -0300

    Delete Lista01_exe03.py

--- Lista01_exe03.py
+++
@@ -1,6 +0,0 @@
-# Ler uma temperatura em graus Fahrenheit, desenvolver um algoritmo usando fluxograma para converter e imprimir a temperatura em graus Celsius.  
-#Fórmula de conversão: Temperatura em Celsius = (Temperatura em Fahrenheit – 32)*(5 / 9).
-temperatura = float(input('informe a temperatura em fahrenheit:'))
-
-conversao = (temperatura - 32)*(5/9)
-print('a temperatura em celsius eh:', conversao)
