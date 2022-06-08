e447187390e7e97fab4b4b4108b866240ee208dc
Maria Eduarda Claro <79665660+Maria-Eduarda-Claro@users.noreply.github.com>
2022-06-08 19:11:17 -0300

    Add files via upload

---
+++ FOLDER/lista02_exe01.py
@@ -0,0 +1,4 @@
+#Escrever um algoritmo usando fluxograma que leia o código do item pedido, a quantidade e  calcule o valor a ser pago por aquele lanche.  
+
+codigo = float(input('informe o codigo do produto:'))
+quantidade = float(input('informe a quantidade do produto:'))
