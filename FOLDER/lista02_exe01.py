4d3e430d7b074138d69ea2ad512de965e267a5f5
Maria Eduarda Claro <79665660+Maria-Eduarda-Claro@users.noreply.github.com>
2022-06-08 19:13:27 -0300

    Delete FOLDER directory

--- FOLDER/lista02_exe01.py
+++
@@ -1,4 +0,0 @@
-#Escrever um algoritmo usando fluxograma que leia o código do item pedido, a quantidade e  calcule o valor a ser pago por aquele lanche.  
-
-codigo = float(input('informe o codigo do produto:'))
-quantidade = float(input('informe a quantidade do produto:'))
