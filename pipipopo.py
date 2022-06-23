17698ee5e746bc36ed82d520cdc6d05b26fe5a4d
Maria Eduarda Claro <79665660+Maria-Eduarda-Claro@users.noreply.github.com>
2022-06-22 22:05:55 -0300

    Add files via upload
    Programa besta, que fiz com o único objetivo de incomodar minha namorada, enquanto eu deveria estar prestando atenção na aula!
    É isso, programa de milhões :)b

---
+++ pipipopo.py
@@ -0,0 +1,9 @@
+mensagem = str(input('A Amanda eh chata?(S or N:'))
+
+if mensagem == 'S':
+    print('Amanda, você é chata, mas a Maria te ama!', mensagem)
+elif mensagem == 'N':
+    print('Considere está uma opção impossível.')
+
+
+
